a9b22283f2037a26073e6c463d95758422e9b9e8
McKinney family <mckinneyfamily@Macs-MacBook-Air.local>
2018-04-09 00:17:37 -0400

    first commit

---
+++ SipZ!/listTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  listTableViewCell.swift
+//  SipZ!
+//
+//  Created by McKinney family  on 4/5/18.
+//  Copyright © 2018 FasTek Technologies. All rights reserved.
+//
+
+import UIKit
+
+class listTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
